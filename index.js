2f2710e57ef3427fc10bfe93e35cb40635865c58
Maycon <mayconrebordao2014@gmail.com>
2018-06-20 13:55:30 -0300

    Teste GitLab CI

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ setInterval(() => {
 
 // console.log();
 
-const port = () => process.env.PORT || 3000;
+const port = () => process.env.PORT || 8080;
 
 app.listen(port(), () => {
     // console.log(port());
